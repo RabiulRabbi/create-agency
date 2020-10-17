b347c9c9f53478f7329e168c6e1d6451b9f7683b
Your Name <you@example.com>
2020-10-17 22:22:37 +0600

    first commit

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyDWpjieVDxoytqzxSKsoK97F0LGM2k_8PE",
+    authDomain: "creative-agency-650cf.firebaseapp.com",
+    databaseURL: "https://creative-agency-650cf.firebaseio.com",
+    projectId: "creative-agency-650cf",
+    storageBucket: "creative-agency-650cf.appspot.com",
+    messagingSenderId: "589400699988",
+    appId: "1:589400699988:web:337c071e56800a2cbbe9c1"
+};
